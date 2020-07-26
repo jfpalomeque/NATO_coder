73689efe90d5f541bce2dc01568240a4ac5d5706
jfpalomeque <jfpalomeque.gonzalez@gmail.com>
2020-07-26 21:33:02 +0100

    first commit

---
+++ nato_coder_oneline.py
@@ -0,0 +1,43 @@
+nato_codes = {
+    'A' : 'Alpha',
+    'B' : 'Bravo',
+    'C' : 'Charlie',
+    'D' : 'Delta',
+    'E' : 'Echo',
+    'F' : 'Foxtrot',
+    'G' : 'Golf',
+    'H' : 'Hotel',
+    'I' : 'India',
+    'J' : 'Juliet',
+    'K' : 'Kilo',
+    'L' : 'Lima',
+    'M' : 'Mike',
+    'N' : 'November',
+    'O' : 'Oscar',
+    'P' : 'Papa',
+    'Q' : 'Quebec',
+    'R' : 'Romeo',
+    'S' : 'Sierra',
+    'T' : 'Tango',
+    'U' : 'Uniform',
+    'V' : 'Victor',
+    'W' : 'Whisky',
+    'X' : 'X-ray',
+    'Y' : 'Yankee',
+    'Z' : 'Zulu',
+    ' ' : 'Space'
+}
+
+print('Welcome to  the NATO coder app with one-line output. Please, enter the word or words to code in NATO phonetic alphabet. Then, press ENTER')
+input = input()
+print('....................................................................................................')
+
+input = input.upper()
+output = list()
+
+for ch in range(len(input)):
+    key = input[ch]
+    output.append(nato_codes[key])
+
+print(output)
+    
---
+++ nato_coder_sound.py
@@ -0,0 +1,47 @@
+import winsound
+
+nato_codes = {
+    'A' : 'Alpha',
+    'B' : 'Bravo',
+    'C' : 'Charlie',
+    'D' : 'Delta',
+    'E' : 'Echo',
+    'F' : 'Foxtrot',
+    'G' : 'Golf',
+    'H' : 'Hotel',
+    'I' : 'India',
+    'J' : 'Juliet',
+    'K' : 'Kilo',
+    'L' : 'Lima',
+    'M' : 'Mike',
+    'N' : 'November',
+    'O' : 'Oscar',
+    'P' : 'Papa',
+    'Q' : 'Quebec',
+    'R' : 'Romeo',
+    'S' : 'Sierra',
+    'T' : 'Tango',
+    'U' : 'Uniform',
+    'V' : 'Victor',
+    'W' : 'Whisky',
+    'X' : 'X-ray',
+    'Y' : 'Yankee',
+    'Z' : 'Zulu',
+    ' ' : 'Space'
+}
+
+print('Welcome to  the NATO coder app with sound. Please, enter the word or words to code in NATO phonetic alphabet. Then, press ENTER')
+input = input()
+print('....................................................................................................')
+
+input = input.upper()
+
+for ch in range(len(input)):
+    key = input[ch]
+    print(nato_codes[key])
+    if key == ' ':
+        sound = ('sounds/silence.wav')
+    else:
+        sound = ('sounds/' + key + '.wav')
+    winsound.PlaySound(sound, winsound.SND_NOSTOP)
+    
